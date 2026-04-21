9a1b5d751e251770aeef4841da15c0a128702535
shevmaryko <mariia.shev@mail.ru>
2026-04-21 17:01:09 +0500

    change sequence to (0,1)

--- sit.py
+++ sit.py
@@ -1,5 +1,6 @@
 def fib(n):
     if n <= 0:return 0
-    elif n == 1 or n == 2:return 1
+    elif n == 1:return 0
+    elif n == 2:return 1
     else:return fib(n-1) + fib(n-2)
-print(fib(10)) #55
+print(fib(10)) #34
